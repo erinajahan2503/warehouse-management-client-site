8a07e14b8654281cc4a3dfe6fa099c2fb2adace7
erina jahan <erinajahan3216@gmail.com>
2022-05-07 16:07:02 +0600

    componenet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Banner from './Pages/Home/Banner/Banner';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home/Home';
+import Services from './Pages/Home/Services/Services';
+import Header from './Pages/Shared/Header/Header';
+import Notfound from './Pages/Notfound/Notfound';
+import Blog from './Pages/Blog/Blog';
+import CheckOut from './Pages/CheckOut/CheckOut';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
+
 
 function App() {
   return (
-    <div className="App">
-      <Banner></Banner>
+    <div className='App'>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/service" element={<Services></Services>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/checkout" element={<RequireAuth>
+          <CheckOut></CheckOut>
+        </RequireAuth>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="*" element={<Notfound></Notfound>}></Route>
+      </Routes>
+
     </div>
   );
 }
 
 export default App;
+
