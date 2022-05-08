bff2cd0ae2c5d41354b58e4846fb719206c845ee
erina jahan <erinajahan3216@gmail.com>
2022-05-08 21:42:28 +0600

    restarting again

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Blog from './Pages/Blog/Blog';
-import Home from './Pages/Home/Home/Home';
-import Login from './Pages/Login/Login/Login';
-import Register from './Pages/Login/Register/Register';
-import ManageInventory from './Pages/ManageInventory/ManageInventory';
-import Notfound from './Pages/Notfound/Notfound';
-import Header from './Pages/Shared/Header/Header';
-
-
-
 
 function App() {
   return (
-    <div className='App'>
-
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/blog' element={<Blog></Blog>}></Route>
-        <Route path='/manageinventory' element={<ManageInventory></ManageInventory>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
-        <Route path="*" element={<Notfound></Notfound>}></Route>
-      </Routes>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
