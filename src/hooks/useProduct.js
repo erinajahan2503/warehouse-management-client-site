847daab3100526e4ebb186c5aece3ce858a68a9a
erina jahan <erinajahan3216@gmail.com>
2022-05-09 12:23:54 +0600

    header

---
+++ src/hooks/useProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const useProduct = () => {
+
+    return (
+        <div>
+            <h1>This is profucts page</h1>
+        </div>
+    );
+};
+
+export default useProduct;
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,55 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import logo from '../../../images/logo/car.png'
+
+const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="info" variant="light">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img height={30} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="home">Home</Nav.Link>
+                            <Nav.Link href="home#inventoryitems">Items</Nav.Link>
+                            <Nav.Link href="home#experts">Blogs</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
+                            {
+                                user && <>
+                                    <Nav.Link as={Link} to="manage">Manage Inventory</Nav.Link>
+                                    <Nav.Link as={Link} to="addservice">Add Item</Nav.Link>
+                                    <Nav.Link as={Link} to="addservice">My Items</Nav.Link>
+
+                                </>
+                            }
+                            {
+                                user ?
+                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
+                                    :
+                                    <Nav.Link as={Link} to="login">
+                                        Login
+                                    </Nav.Link>}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
